dac70fd8101a19f47202c740f14634a1809039fd
ololx <ololx@icloud.com>
2022-02-14 12:01:50 +0500

    feature(task62.*) add solution for task 62]

---
+++ src/test/java/io/github/ololx/leetcode/examples/medium/task62/SolutionTest.java
@@ -0,0 +1,29 @@
+package io.github.ololx.leetcode.examples.medium.task62;
+
+import io.github.ololx.leetcode.examples.medium.task62.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 14.02.2022 10:38
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "fieldsAndPaths")
+    public static Object[][] fieldsAndPaths() {
+        return new Object[][] {
+                {3, 2, 3},
+                {3, 7, 28}
+        };
+    }
+
+    @Test(dataProvider = "fieldsAndPaths")
+    public void intToRoman_whenIntegerIsDefine_thenReturnNonZeroRomanNumber(int m, int n, int pathsCount) {
+        assertEquals(new Solution().uniquePaths(m, n), pathsCount);
+    }
+}
