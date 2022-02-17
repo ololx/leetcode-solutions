9d097d8891d6ad2478d7a2aebf1bbe4267260b3c
ololx <ololx@icloud.com>
2022-02-17 14:02:30 +0500

    feature(task14.*) addd solution for task 14

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task14/SolutionTest.java
@@ -0,0 +1,29 @@
+package io.github.ololx.leetcode.examples.easy.task14;
+
+import io.github.ololx.leetcode.examples.easy.task14.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 17.02.2022 13:49
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "stringsAndPrefix")
+    public static Object[][] stringsAndPrefix() {
+        return new Object[][] {
+                {new String[] {"flower","flow","flight"}, "fl"},
+                {new String[] {"dog","racecar","car"}, ""}
+        };
+    }
+
+    @Test(dataProvider = "stringsAndPrefix")
+    public void romanToInt_whenRomanNumberIsDefine_thenReturnNonZeroIntegerNumber(String[] strs , String prefix) {
+        assertEquals(new Solution().longestCommonPrefix(strs), prefix);
+    }
+}
