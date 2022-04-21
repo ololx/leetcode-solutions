b4dd5b23e3c527dccb7afc096e93953e66513eb6
ololx <ololx@icloud.com>
2022-04-21 23:05:19 +0500

    chore(task27.*) init solution for task 27

---
+++ src/main/java/io/github/ololx/leetcode/examples/easy/task27/Solution.java
@@ -0,0 +1,25 @@
+package io.github.ololx.leetcode.examples.easy.task27;
+
+import java.util.Stack;
+
+/**
+ * project leetcode-solutions
+ * created 21.04.2022 22:53
+ *
+ * @author Alexander A. Kropotin
+ */
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (val != num) {
+                nums[count] = num;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
