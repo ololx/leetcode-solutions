ba66e64b044763c173aa1f88bd6e8ee9c2579b1c
ololx <ololx@icloud.com>
2022-05-06 14:32:17 +0500

    feature(task231.*) add solution for task 231 version 2

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task231/SolutionV2Test.java
@@ -0,0 +1,37 @@
+package io.github.ololx.leetcode.examples.easy.task231;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 06.05.2022 14:31
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionV2Test {
+
+    private final static Logger log = Logger.getLogger(SolutionV2Test.class.getCanonicalName());
+
+    @DataProvider(name = "providesNums")
+    public static Object[][] providesNums() {
+        return new Object[][] {
+                {1, true},
+                {16, true},
+                {3, false},
+                {-2147483648, false},
+        };
+    }
+
+    @Test(dataProvider = "providesNums")
+    public void isPowerOfTwo_whenNumIsPowerOf2_thenReturnTrue(int n , boolean expected) {
+        boolean actual = new Solution().isPowerOfTwo(n);
+        log.info("Expected = " + expected + " & actual = " + actual);
+
+        assertEquals(actual, expected);
+    }
+}
