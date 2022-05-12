110ec16b60013f4558e0f947c9fb99397b07eb4f
ololx <ololx@icloud.com>
2022-05-12 21:10:57 +0500

    feature(task136.*) add solution version 2 for task 136

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task136/SolutionV2Test.java
@@ -0,0 +1,29 @@
+package io.github.ololx.leetcode.examples.easy.task136;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 12.05.2022 21:10
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionV2Test {
+
+    @DataProvider(name = "nums")
+    public static Object[][] nums() {
+        return new Object[][] {
+                {new int[]{2,2,1}, 1},
+                {new int[]{4,1,2,1,2}, 4},
+                {new int[]{1}, 1}
+        };
+    }
+
+    @Test(dataProvider = "nums")
+    public void singleNumber_whenArrayIsNotEmpty_thenReturnUniqueNumber(int[] nums, int singleNumber) {
+        assertEquals(new SolutionV2().singleNumber(nums), singleNumber);
+    }
+}
