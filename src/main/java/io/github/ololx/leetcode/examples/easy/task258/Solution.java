4409d94196e3f98f54cd75c5338a5e6d7a608214
ololx <ololx@icloud.com>
2022-05-16 11:36:38 +0500

    feature(task258.*) add solution for task 258

---
+++ src/main/java/io/github/ololx/leetcode/examples/easy/task258/Solution.java
@@ -0,0 +1,40 @@
+package io.github.ololx.leetcode.examples.easy.task258;
+
+/**
+ * 258. Add Digits
+ *
+ * Given an integer num, repeatedly add all its digits until
+ * the result has only one digit, and return it.
+ *
+ * Example 1:
+ * <p>Input: num = 38
+ * Output: 2
+ * Explanation: The process is
+ * 38 --> 3 + 8 --> 11
+ * 11 --> 1 + 1 --> 2
+ * Since 2 has only one digit, return it.</p>
+ *
+ * Example 2:
+ * <p>Input: num = 0
+ * Output: 0</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          0 <= num <= 2^31 - 1
+ *      </li>
+ * </ul>
+ *
+ * Follow up: Could you do it without any loop/recursion in O(1) runtime?
+ *
+ * project leetcode-solutions
+ * created 16.05.2022 11:21
+ *
+ * @author Alexander A. Kropotin
+ */
+public class Solution {
+
+    public int addDigits(int num) {
+        return num == 0 ? 0 : 1 + (--num % 9);
+    }
+}
---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task258/SolutionTest.java
@@ -0,0 +1,29 @@
+package io.github.ololx.leetcode.examples.easy.task258;
+
+import io.github.ololx.leetcode.examples.easy.task258.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 16.05.2022 11:25
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "providesNums")
+    public static Object[][] providesNums() {
+        return new Object[][] {
+                {38, 2},
+                {0, 0}
+        };
+    }
+
+    @Test(dataProvider = "providesNums")
+    public void addDigits_whenNumHigherZero_thenReturnDigitalRoot(int n, int expected) {
+        assertEquals(new Solution().addDigits(n), expected);
+    }
+}
