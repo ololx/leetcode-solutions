d46a12aec0287046245bc44da8054b2f497a8688
ololx <ololx@icloud.com>
2022-05-27 18:57:14 +0500

    feature(task2169.*) add solution for task 2169

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task2169/SolutionTest.java
@@ -0,0 +1,31 @@
+package io.github.ololx.leetcode.examples.easy.task2169;
+
+import io.github.ololx.leetcode.examples.easy.task2169.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 27.05.2022 18:47
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "providesNum1AndNum2")
+    public static Object[][] providesNum1AndNum2() {
+        return new Object[][] {
+                {2, 3, 3},
+                {10, 10, 1}
+        };
+    }
+
+    @Test(dataProvider = "providesNum1AndNum2")
+    public void countOperations_whenNum1IsNotEqualNum2_thenReturnMoreOneCount(int num1,
+                                                                              int num2,
+                                                                              int expected) {
+        assertEquals(new Solution().countOperations(num1, num2), expected);
+    }
+}
