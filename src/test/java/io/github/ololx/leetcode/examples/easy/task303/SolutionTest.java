27889fe0ecaf99d973ba2e982e11cae442cbcce3
ololx <ololx@icloud.com>
2022-06-03 23:43:06 +0500

    feature(task303.*) add solution test for task 303

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task303/SolutionTest.java
@@ -0,0 +1,24 @@
+package io.github.ololx.leetcode.examples.easy.task303;
+
+import io.github.ololx.leetcode.examples.easy.task303.Solution;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 03.06.2022 23:38
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @Test
+    public void sumRange_whenRangeIsNotEmpty_thenReturnSumOfRangeHigherThanZero() {
+        Solution.NumArray numArray = new Solution.NumArray(new int[] {-2, 0, 3, -5, 2, -1});
+
+        assertEquals(numArray.sumRange(0, 2), 1);
+        assertEquals(numArray.sumRange(2, 5), -1);
+        assertEquals(numArray.sumRange(0, 5), -3);
+    }
+}
