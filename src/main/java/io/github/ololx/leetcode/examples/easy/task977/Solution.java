a3d602497e46f32f72c03ffec02bd9a905b4d039
ololx <ololx@icloud.com>
2022-06-08 20:14:13 +0500

    feature(task977.*) init solution for task 977

---
+++ src/main/java/io/github/ololx/leetcode/examples/easy/task977/Solution.java
@@ -0,0 +1,33 @@
+package io.github.ololx.leetcode.examples.easy.task977;
+
+import java.util.Stack;
+
+/**
+ * project leetcode-solutions
+ * created 07.06.2022 14:27
+ *
+ * @author Alexander A. Kropotin
+ */
+public class Solution {
+
+    public int[] sortedSquares(int[] nums) {
+        int currPointer = 1, lastMinPointer = 0;
+        nums[lastMinPointer] = nums[lastMinPointer] * nums[lastMinPointer];
+
+        while (lastMinPointer <= currPointer && currPointer < nums.length) {
+            nums[currPointer] = nums[currPointer] * nums[currPointer];
+
+            if (nums[currPointer] <= nums[lastMinPointer]) {
+                nums[currPointer] ^= nums[lastMinPointer];
+                nums[lastMinPointer] ^= nums[currPointer];
+                nums[currPointer] ^= nums[lastMinPointer];
+
+                lastMinPointer = currPointer;
+            }
+
+            currPointer++;
+        }
+
+        return nums;
+    }
+}
