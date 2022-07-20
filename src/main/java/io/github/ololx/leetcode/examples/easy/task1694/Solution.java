576efc508f518c27d54c8907a95e42cc29a6d76c
ololx <ololx@icloud.com>
2022-07-20 13:35:05 +0500

    chore(task1694.*) managed solution for task 1694

--- src/main/java/io/github/ololx/leetcode/examples/easy/task1694/Solution.java
+++ src/main/java/io/github/ololx/leetcode/examples/easy/task1694/Solution.java
@@ -1,6 +1,75 @@
 package io.github.ololx.leetcode.examples.easy.task1694;
 
 /**
+ * 1694. Reformat Phone Number
+ *
+ * You are given a phone number as a string number. number consists of digits,
+ * spaces ' ', and/or dashes '-'.
+ *
+ * You would like to reformat the phone number in a certain manner.
+ * Firstly, remove all spaces and dashes. Then, group the digits from left to right
+ * into blocks of length 3 until there are 4 or fewer digits. The final digits are
+ * then grouped as follows:
+ * <ul>
+ *     <ol>
+ *         2 digits: A single block of length 2.
+ *     </ol>
+ *     <ol>
+ *         3 digits: A single block of length 3.
+ *     </ol>
+ *     <ol>
+ *         4 digits: Two blocks of length 2 each.
+ *     </ol>
+ * </ul>
+ *
+ * The blocks are then joined by dashes. Notice that the reformatting process
+ * should never produce any blocks of length 1 and produce at most two blocks of length 2.
+ *
+ * Return the phone number after formatting.
+ *
+ * Example 1:
+ * <p>Input: number = "1-23-45 6"
+ * Output: "123-456"
+ * Explanation: The digits are "123456".
+ * Step 1: There are more than 4 digits, so group the next 3 digits.
+ * The 1st block is "123".
+ * Step 2: There are 3 digits remaining, so put them in a single block of length 3.
+ * The 2nd block is "456".
+ * Joining the blocks gives "123-456".</p>
+ *
+ * Example 2:
+ * <p>Input: number = "123 4-567"
+ * Output: "123-45-67"
+ * Explanation: The digits are "1234567".
+ * Step 1: There are more than 4 digits, so group the next 3 digits.
+ * The 1st block is "123".
+ * Step 2: There are 4 digits left, so split them into two blocks of length 2.
+ * The blocks are "45" and "67".
+ * Joining the blocks gives "123-45-67".</p>
+ *
+ * Example 3:
+ * <p>Input: number = "123 4-5678"
+ * Output: "123-456-78"
+ * Explanation: The digits are "12345678".
+ * Step 1: The 1st block is "123".
+ * Step 2: The 2nd block is "456".
+ * Step 3: There are 2 digits left, so put them in a single block of length 2.
+ * The 3rd block is "78".
+ * Joining the blocks gives "123-456-78".</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          2 <= number.length <= 100
+ *      </li>
+ *      <li>
+ *          number consists of digits and the characters '-' and ' '.
+ *      </li>
+ *      <li>
+ *          There are at least two digits in number.
+ *      </li>
+ * </ul>
+ *
  * project leetcode-solutions
  * created 23.02.2022 13:10
  *
