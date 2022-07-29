f534ed85abc4ff6eeda6c3fd38581870f3b86e64
ololx <ololx@icloud.com>
2022-07-29 20:28:58 +0500

    chore(SolutionTest.java) add tests

---
+++ src/test/java/io/github/ololx/leetcode/examples/easy/task2000/SolutionTest.java
@@ -0,0 +1,32 @@
+package io.github.ololx.leetcode.examples.easy.task2000;
+
+import io.github.ololx.leetcode.examples.easy.task2000.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 29.07.2022 19:44
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "providesStrings")
+    public static Object[][] providesStrings() {
+        return new Object[][] {
+                {"abcdefd", 'd', "dcbaefd"},
+                {"xyxzxe", 'z', "zxyxxe"},
+                {"abcd", 'z', "abcd"},
+        };
+    }
+
+    @Test(dataProvider = "providesStrings")
+    public void reversePrefix_whenWordAndChIsNotEmpty_thenReverseWord(String word ,
+                                                                      char ch,
+                                                                      String expected) {
+        assertEquals(new Solution().reversePrefix(word, ch), expected);
+    }
+}
