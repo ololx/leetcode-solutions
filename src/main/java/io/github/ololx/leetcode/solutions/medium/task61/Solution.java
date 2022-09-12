f0d9d7f34e223657989f7cdb5fdca1973725c6f5
ololx <ololx@icloud.com>
2022-09-12 13:16:13 +0500

    chore(*) add description for task61 and fix link to solution

--- src/main/java/io/github/ololx/leetcode/solutions/medium/task61/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task61/Solution.java
@@ -1,6 +1,31 @@
 package io.github.ololx.leetcode.solutions.medium.task61;
 
 /**
+ * 61. Rotate List
+ *
+ * Given the head of a linked list, rotate the list to the right by k places.
+ *
+ * Example 1:
+ * <p>Input: head = [1,2,3,4,5], k = 2
+ * Output: [4,5,1,2,3]</p>
+ *
+ * Example 2:
+ * <p>Input: head = [0,1,2], k = 4
+ * Output: [2,0,1]</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          The number of nodes in the list is in the range [0, 500].
+ *      </li>
+ *      <li>
+ *          -100 <= Node.val <= 100
+ *      </li>
+ *      <li>
+ *          0 <= k <= 2 * 10^9
+ *      </li>
+ * </ul>
+ *
  * project leetcode-solutions
  * created 11.03.2022 21:55
  *
