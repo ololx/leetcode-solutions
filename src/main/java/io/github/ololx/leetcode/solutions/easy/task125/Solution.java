e1951c3b77a5f096693972bffe12c20570e8d5fa
ololx <ololx@icloud.com>
2022-10-26 11:52:15 +0500

    chore(*) add tests for solution

--- src/main/java/io/github/ololx/leetcode/solutions/easy/task125/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/easy/task125/Solution.java
@@ -3,6 +3,38 @@ package io.github.ololx.leetcode.solutions.easy.task125;
 /**
  * 125. Valid Palindrome
  *
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+ * and removing all non-alphanumeric characters, it reads the same forward and backward.
+ * Alphanumeric characters include letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Example1 :
+ * <p>Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.</p>
+ *
+ * Example 2:
+ * <p>Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.</p>
+ *
+ * Example 3:
+ * <p>Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.</p>
+ *
+ * Constraints:
+ * <ul>
+ *     <li>
+ *         1 <= s.length <= 2 * 10^5
+ *     </li>
+ *     <li>
+ *         s consists only of printable ASCII characters.
+ *     </li>
+ * </ul>
+ *
  * project leetcode-solutions
  * created 25.10.2022 23:27
  *
