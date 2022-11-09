b4d16465b85fbfe9a08bb857302aba9cd19655e6
ololx <ololx@icloud.com>
2022-11-09 21:21:07 +0500

    chore(*) add tests

---
+++ src/test/java/io/github/ololx/leetcode/solutions/easy/task434/SolutionTest.java
@@ -0,0 +1,31 @@
+package io.github.ololx.leetcode.solutions.easy.task434;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 09.11.2022 21:17
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "providesStrings")
+    public static Object[][] providesStrings() {
+        return new Object[][] {
+                {"Hello, my name is John", 5},
+                {"Hello", 1},
+                {"             ", 0}
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "providesStrings")
+    public void countSegments_whenStringHasWords_thenReturnCount(String s, int expected) {
+        assertEquals(new Solution().countSegments(s), expected);
+    }
+}
