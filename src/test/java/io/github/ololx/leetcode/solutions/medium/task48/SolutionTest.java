a6b930f4eeffe6432508d4940a756d48bf1092f4
ololx <ololx@icloud.com>
2022-11-15 21:31:26 +0200

    chore(*) add tests for solution

---
+++ src/test/java/io/github/ololx/leetcode/solutions/medium/task48/SolutionTest.java
@@ -0,0 +1,42 @@
+package io.github.ololx.leetcode.solutions.medium.task48;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import io.github.ololx.leetcode.solutions.medium.task48.Solution;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 15.11.2022 13:28
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "provides2Ds")
+    public static Object[][] provides2Ds() {
+        return new Object[][] {
+                {
+                    new int[][] {
+                            new int[] {1, 2, 3},
+                            new int[] {4, 5, 6},
+                            new int[] {7, 8, 9}
+                    },
+                        new int[][] {
+                                new int[] {7, 4, 1},
+                                new int[] {8, 5, 2},
+                                new int[] {9, 6, 3}
+                        }
+                }
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "provides2Ds")
+    public void rotate_whenMatrixExists_thenRotateOn90Degree(int[][] matrix, int[][] expected) {
+        new Solution().rotate(matrix);
+        assertEquals(matrix, expected);
+    }
+}
