4276d55a870399de98dae07f49b71d0514e40a90
ololx <ololx@icloud.com>
2022-11-19 17:09:13 +0200

    chore(SolutionTest.java) complete tests

--- src/test/java/io/github/ololx/leetcode/solutions/medium/task24/SolutionTest.java
+++ src/test/java/io/github/ololx/leetcode/solutions/medium/task24/SolutionTest.java
@@ -1,7 +1,7 @@
 package io.github.ololx.leetcode.solutions.medium.task24;
 
 import io.github.ololx.cranberry.logging.annotation.LogParam;
-import io.github.ololx.leetcode.solutions.medium.task24.Solution;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -14,11 +14,46 @@ import static org.testng.Assert.assertEquals;
  */
 public class SolutionTest {
 
+    @DataProvider(name = "providesListNodes")
+    public static Object[][] providesListNodes() {
+        return new Object[][] {
+                {
+                        new Solution.ListNode(
+                                1,
+                                new Solution.ListNode(
+                                        2,
+                                        new Solution.ListNode(
+                                                3,
+                                                new Solution.ListNode(4)
+                                        )
+                                )
+                        ),
+                        new Solution.ListNode(
+                                2,
+                                new Solution.ListNode(
+                                        1,
+                                        new Solution.ListNode(
+                                                4,
+                                                new Solution.ListNode(1)
+                                        )
+                                )
+                        ),
+                },
+                {
+                        new Solution.ListNode(),
+                        new Solution.ListNode(),
+                },
+                {
+                        new Solution.ListNode(1),
+                        new Solution.ListNode(1),
+                }
+        };
+    }
+
     @LogParam
-    @Test
-    public void test() {
-        Solution.ListNode head = new Solution.ListNode(1, new Solution.ListNode(2, new Solution.ListNode(3, new Solution.ListNode(4))));
-        Solution.ListNode expected = new Solution.ListNode(2, new Solution.ListNode(1, new Solution.ListNode(4, new Solution.ListNode(1))));
+    @Test(dataProvider = "providesListNodes")
+    public void swapPairs_whenPairsExists_thenSwapListNodes(Solution.ListNode head,
+                                                            Solution.ListNode expected) {
         assertEquals(new Solution().swapPairs(head), expected);
     }
 }
