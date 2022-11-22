bfc88529ee094939a0770ea527b1595b45a160ea
ololx <ololx@icloud.com>
2022-11-22 10:04:03 +0200

    feature(Solution.java) add solution for task

---
+++ src/main/java/io/github/ololx/leetcode/solutions/easy/task1534/Solution.java
@@ -0,0 +1,28 @@
+package io.github.ololx.leetcode.solutions.easy.task1534;
+
+/**
+ * project leetcode-solutions
+ * created 22/11/2022 10:02
+ *
+ * @author Alexander A. Kropotin
+ */
+public class Solution {
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int unequalsCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a
+                            && Math.abs(arr[j] - arr[k]) <= b
+                            && Math.abs(arr[i] - arr[k]) <= c) {
+                        unequalsCount++;
+                    }
+                }
+            }
+        }
+
+        return unequalsCount;
+    }
+}
