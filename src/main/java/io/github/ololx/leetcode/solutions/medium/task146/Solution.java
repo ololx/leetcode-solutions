8e36aca2fb7be13feba4985e348c607efde452e1
ololx <ololx@icloud.com>
2023-01-29 22:14:35 +0200

    little bit refactor

--- src/main/java/io/github/ololx/leetcode/solutions/medium/task146/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task146/Solution.java
@@ -8,6 +8,12 @@ import java.util.Map;
 /**
  * 146. LRU Cache
  *
+ * @apiNote
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ *
  * project leetcode-solutions
  * created 29.01.2023 15:45
  * <p>
@@ -17,13 +23,13 @@ public class Solution {
 
     static class LRUCache {
 
-        private final int capacity;
-
         private final Map<Integer, CachedNode<Integer, Integer>> elements;
 
         private CachedNode<Integer, Integer> head = new CachedNode<>(null, null);
 
         private CachedNode<Integer, Integer> tail = new CachedNode<>(null, null);
+
+        private final int capacity;
 
         public LRUCache(int capacity) {
             this.capacity = capacity;
@@ -33,38 +39,51 @@ public class Solution {
         }
 
         public int get(int key) {
-            CachedNode<Integer, Integer> mruNode = this.elements.get(key);
-            if (mruNode == null) {
+            CachedNode<Integer, Integer> currentNode = this.elements.get(key);
+            if (currentNode == null) {
                 return -1;
             }
 
-            mruNode.next.previous = mruNode.previous;
-            mruNode.previous.next = mruNode.next;
+            moveNodeToHead(currentNode);
 
-            mruNode.next = this.head.next;
-            mruNode.previous = this.head;
-            this.head.next.previous = mruNode;
-            this.head.next = mruNode;
+            return currentNode.value;
+        }
 
-            return mruNode.value;
+        private void moveNodeToHead(CachedNode<Integer, Integer> node) {
+            CachedNode<Integer, Integer> previousNode = node.previous;
+            CachedNode<Integer, Integer> nextNode = node.next;
+            nextNode.previous = node.previous;
+            previousNode.next = node.next;
+
+            CachedNode<Integer, Integer> recentlyUsedNode = this.head.next;
+            node.next = recentlyUsedNode;
+            recentlyUsedNode.previous = node;
+
+            node.previous = this.head;
+            this.head.next = node;
         }
 
         public void put(int key, int value) {
             if (this.elements.containsKey(key)) {
                 CachedNode<Integer, Integer> currentNode = this.elements.remove(key);
-                currentNode.previous.next = currentNode.next;
-                currentNode.next.previous = currentNode.previous;
-            } else if (this.elements.size() == this.capacity) {
-                CachedNode<Integer, Integer> lruNode = this.tail.previous;
-                this.tail.previous = lruNode.previous;
-                lruNode.previous.next = this.tail;
-                this.elements.remove(lruNode.key);
+                removeNode(currentNode);
+            } else if (this.capacity == this.elements.size()) {
+                CachedNode<Integer, Integer> lastUsedNode = this.tail.previous;
+                removeNode(lastUsedNode);
+                this.elements.remove(lastUsedNode.key);
             }
 
             CachedNode<Integer, Integer> cachedNode = new CachedNode<>(key, value, this.head, this.head.next);
             this.head.next.previous = cachedNode;
             this.head.next = cachedNode;
             this.elements.put(key, cachedNode);
+        }
+
+        private void removeNode(CachedNode<Integer, Integer> node) {
+            CachedNode<Integer, Integer> previousNode = node.previous;
+            CachedNode<Integer, Integer> nextNode = node.next;
+            nextNode.previous = node.previous;
+            previousNode.next = node.next;
         }
 
         private class CachedNode<K, V> {
