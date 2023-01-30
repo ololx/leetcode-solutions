7ef656e7a0179ff63314c56f4898792e444433db
a-kropotin <a.kropotin@pixonic.com>
2023-01-30 10:14:25 +0200

    add description

--- src/main/java/io/github/ololx/leetcode/solutions/medium/task146/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task146/Solution.java
@@ -1,12 +1,70 @@
 package io.github.ololx.leetcode.solutions.medium.task146;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 /**
  * 146. LRU Cache
+ *
+ * Design a data structure that follows the constraints of a Least Recently
+ * Used (LRU) cache.
+ *
+ * Implement the LRUCache class:
+ * <ol>
+ *     <lo>
+ *         LRUCache(int capacity) Initialize the LRU cache with positive
+ *         size capacity.
+ *     </lo>
+ *     <lo>
+ *         int get(int key) Return the value of the key if the key exists,
+ *         otherwise return -1.
+ *     </lo>
+ *     <li>
+ *         void put(int key, int value) Update the value of the key if
+ *         the key exists. Otherwise, add the key-value pair to the cache.
+ *         If the number of keys exceeds the capacity from this operation,
+ *         evict the least recently used key.
+ *     </li>
+ *     <li>
+ *         The functions get and put must each run in O(1) average
+ *         time complexity.
+ *     </li>
+ * </ol>
+ *
+ * Example 1:
+ * <p>Input
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * Output
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ *
+ * Explanation
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // cache is {1=1}
+ * lRUCache.put(2, 2); // cache is {1=1, 2=2}
+ * lRUCache.get(1);    // return 1
+ * lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+ * lRUCache.get(2);    // returns -1 (not found)
+ * lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+ * lRUCache.get(1);    // return -1 (not found)
+ * lRUCache.get(3);    // return 3
+ * lRUCache.get(4);    // return 4</p>
+ *
+ * Constraints:
+ * <ul>
+ *     <li>
+ *         1 <= capacity <= 3000
+ *     </li>
+ *     <li>
+ *         0 <= key <= 10^4
+ *     </li>
+ *     <li>
+ *         0 <= value <= 10^5
+ *     </li>
+ *     <li>
+ *         At most 2 * 105 calls will be made to get and put.
+ *     </li>
+ * </ul>
  *
  * @apiNote
  * Your LRUCache object will be instantiated and called as such:
