8f737a71af6025af675a4a46ef5f803c27157aac
a-kropotin <a.kropotin@pixonic.com>
2023-06-11 10:17:13 +0300

    chore(*) add solution for task 2706

---
+++ src/test/java/io/github/ololx/leetcode/solutions/easy/task2706/SolutionTest.java
@@ -0,0 +1,37 @@
+package io.github.ololx.leetcode.solutions.easy.task2706;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 11.06.2023 10:15
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @LogParam
+    @Test(dataProvider = "providesPricesAndMoney")
+    public void testBuyChoco(int[] prices, int money, int expected) {
+        Solution solution = new Solution();
+        int actual = solution.buyChoco(prices, money);
+
+        assertEquals(actual, expected);
+    }
+
+    @DataProvider
+    public Object[][] providesPricesAndMoney() {
+        return new Object[][] {
+                { new int[] {1, 2, 2}, 3, 0 },
+                { new int[] {3, 2, 3}, 3, 3 },
+                { new int[] {1, 1}, 3, 1 },
+                { new int[] {100, 100}, 200, 0 },
+                { new int[] {10, 20}, 15, 15 }
+        };
+    }
+}
