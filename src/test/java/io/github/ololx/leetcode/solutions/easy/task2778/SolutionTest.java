d64ddfceb19bc9f791ecdc69f597d08a813eaa26
Alexander A. Kropotin <ololx@icloud.com>
2023-08-02 21:57:28 +0300

    chore(*) add new solution for task 2778 (#58)

---
+++ src/test/java/io/github/ololx/leetcode/solutions/easy/task2778/SolutionTest.java
@@ -0,0 +1,30 @@
+package io.github.ololx.leetcode.solutions.easy.task2778;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project leetcode-solutions
+ * created 02.08.2023 21:52
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    @DataProvider
+    public static Object[][] providesNums() {
+        return new Object[][]{
+                {new int[]{1, 2, 3, 4}, 21},
+                {new int[]{2, 7, 1, 19, 18, 3}, 63},
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "providesNums")
+    public void search_whenNumsContainsTarget_thenReturnTarget(int[] nums, int expected) {
+        assertEquals(new Solution().sumOfSquares(nums), expected);
+    }
+}
