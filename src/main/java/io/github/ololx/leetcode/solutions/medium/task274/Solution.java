dbc8dad0685bfa4ab8b781fdb537f77dff9c0b14
Alexander A. Kropotin <ololx@icloud.com>
2023-10-07 23:38:26 +0300

    feature(*) add new solution for task 274 (#61)

---
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task274/Solution.java
@@ -0,0 +1,60 @@
+package io.github.ololx.leetcode.solutions.medium.task274;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 274. H-Index
+ *
+ * Given an array of integers citations where citations[i] is the number of citations a researcher received for their
+ * ith paper, return the researcher's h-index.
+ *
+ * According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the
+ * given researcher has published at least h papers that have each been cited at least h times.
+ *
+ * Example 1:
+ * <p>Input: citations = [3,0,6,1,5]
+ * Output: 3
+ * Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5
+ * citations respectively.
+ * Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations
+ * each, their h-index is 3.</p>
+ *
+ * Example 2:
+ * <p>Input: citations = [1,3,1]
+ * Output: 1</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          n == citations.length
+ *      </li>
+ *      <li>
+ *          1 <= n <= 5000
+ *      </li>
+ *      <li>
+ *          0 <= citations[i] <= 1000
+ *      </li>
+ * </ul>
+ *
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 07/10/2023 4:53 pm
+ */
+public class Solution {
+
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int citationsCount = citations.length;
+        int hIndex = 0;
+
+        for (int citationsIndex = 0; citationsIndex < citationsCount; citationsIndex++) {
+            if (citations[citationsIndex] >= citationsCount - citationsIndex) {
+                hIndex = citationsCount - citationsIndex;
+                break;
+            }
+        }
+
+        return hIndex;
+    }
+}
---
+++ src/test/java/io/github/ololx/leetcode/solutions/medium/task274/SolutionTest.java
@@ -0,0 +1,29 @@
+package io.github.ololx.leetcode.solutions.medium.task274;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 07/10/2023 5:07 pm
+ */
+public class SolutionTest {
+
+    @DataProvider
+    public static Object[][] providesCitations() {
+        return new Object[][]{
+            {new int[]{3, 0, 6, 1, 5}, 3},
+            {new int[]{1, 3, 1}, 1},
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "providesCitations")
+    public void hIndex_whnCitationsMoreOrEqualsPublications_thenReturnCitations(int[] citations, int expected) {
+        assertEquals(new Solution().hIndex(citations), expected);
+    }
+}
