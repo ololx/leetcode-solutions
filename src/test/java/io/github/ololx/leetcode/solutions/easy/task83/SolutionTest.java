3c2cf853bfc6e3e3bc799b0cfb4ee9a4924ce850
a-kropotin <a.kropotin@pixonic.com>
2024-04-16 07:47:30 +0300

    init new solution for task83

---
+++ src/test/java/io/github/ololx/leetcode/solutions/easy/task83/SolutionTest.java
@@ -0,0 +1,51 @@
+package io.github.ololx.leetcode.solutions.easy.task83;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import io.github.ololx.leetcode.solutions.easy.task83.Solution;
+import io.github.ololx.leetcode.solutions.easy.task83.Solution.ListNode;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.util.logging.Logger;
+
+/**
+ * project leetcode-solutions
+ * created 09.03.2022 21:41
+ *
+ * @author Alexander A. Kropotin
+ */
+public class SolutionTest {
+
+    private static final Logger log = Logger.getLogger(SolutionTest.class.getName());
+
+    @DataProvider(name = "providesListNodes")
+    public static Object[][] providesListNodes() {
+        return new Object[][] {
+                {
+                    newInstance(new int[]{1, 1, 2}, 0),
+                        newInstance(new int[]{1, 2}, 0)
+                },
+                {
+                    newInstance(new int[]{1, 1, 2, 3, 3}, 0),
+                    newInstance(new int[]{1, 2, 3}, 0)
+                },
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "providesListNodes")
+    public void deleteDuplicates_whenHeadNodeHasNext_thenReturnNodeWithUniqueValues(ListNode head,
+                                                                                    ListNode expected) {
+        Assert.assertEquals(new Solution().deleteDuplicates(head), expected);
+    }
+
+    private static ListNode newInstance(int[] values, int pos) {
+        if (values == null || values.length == 0 || pos >= values.length) {
+            return null;
+        }
+
+        return new ListNode(values[pos], newInstance(values, pos + 1));
+    }
+}
