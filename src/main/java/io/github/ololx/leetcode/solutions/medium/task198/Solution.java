8f0ba36ced69757e37746ddfc88531aefc6050ad
a-kropotin <a.kropotin@pixonic.com>
2024-05-04 09:35:03 +0300

    feature(Solution.java) add new solution for task 198

---
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task198/Solution.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2024 the project leetcode-solutions authors
+ * and the original author or authors annotated by {@author}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.leetcode.solutions.medium.task198;
+
+/**
+ * 198. House Robber
+ *
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money
+ * stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems
+ * connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you
+ * can rob tonight without alerting the police.
+ *
+ * Example 1:
+ * <p>Input: nums = [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.</p>
+ *
+ * Example 2:
+ * <p>Input: nums = [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          1 <= nums.length <= 100
+ *      </li>
+ *      <li>
+ *          0 <= nums[i] <= 400
+ *      </li>
+ * </ul>
+ *
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 04/05/2024 9:07am
+ */
+class Solution {
+
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        var dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (var houseNum = 2; houseNum < nums.length; houseNum++) {
+            dp[houseNum] = Math.max(dp[houseNum - 1], dp[houseNum - 2] + nums[houseNum]);
+        }
+
+        return dp[dp.length - 1];
+    }
+}
---
+++ src/test/java/io/github/ololx/leetcode/solutions/medium/task198/SolutionTest.java
@@ -0,0 +1,33 @@
+package io.github.ololx.leetcode.solutions.medium.task198;
+
+import io.github.ololx.cranberry.logging.annotation.LogParam;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 04/05/2024 9:07am
+ */
+public class SolutionTest {
+
+    private static final Logger log = Logger.getLogger(SolutionTest.class.getName());
+
+    @DataProvider(name = "providesHouses")
+    public static Object[][] providesHouses() {
+        return new Object[][] {
+            {new int[] {1, 2, 3, 1}, 4},
+            {new int[] {2, 7, 9, 3, 1}, 12},
+        };
+    }
+
+    @LogParam
+    @Test(dataProvider = "providesHouses")
+    public void rob_whenHousesMoreThanOne_thenReturnMaxFromStreet(int[] nums, int expected) {
+        assertEquals(new Solution().rob(nums), expected);
+    }
+}
