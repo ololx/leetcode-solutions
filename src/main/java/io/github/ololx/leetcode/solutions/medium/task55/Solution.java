24596aa7c6ef94676c1c5ad9f5fbb3b3b81c0c65
a-kropotin <a.kropotin@pixonic.com>
2024-05-06 10:09:04 +0300

    feature(Solution.java) add solution for task 55

---
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task55/Solution.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2024 the project leetcode-solutions authors
+ * and the original author or authors annotated by {@author}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.leetcode.solutions.medium.task55;
+
+/**
+ * 55. Jump Game
+ *
+ * You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+ *
+ * Return true if you can reach the last index, or false otherwise.
+ *
+ * Example 1:
+ * <p>Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.</p>
+ *
+ * Example 2:
+ * <p>Input: nums = [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          1 <= nums.length <= 10^4
+ *      </li>
+ *      <li>
+ *          0 <= nums[i] <= 10^5
+ *      </li>
+ * </ul>
+ *
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 06/05/2024 9:50am
+ */
+public class Solution {
+
+    public boolean canJump(int[] nums) {
+        var jumps = 0;
+
+        for (var currentPont = 0; currentPont < nums.length; currentPont++) {
+            if (jumps < currentPont) {
+                return false;
+            } else if (currentPont + nums[currentPont] >= nums.length - 1) {
+                return true;
+            }
+
+            jumps = Math.max(jumps, currentPont + nums[currentPont]);
+        }
+
+        return true;
+    }
+}
