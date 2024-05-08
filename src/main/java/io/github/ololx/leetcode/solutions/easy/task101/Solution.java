77770d3383ae46fa475d2b9372ba581473aed2f7
a-kropotin <a.kropotin@pixonic.com>
2024-05-08 15:36:41 +0300

    chore(*) add tests && docs

--- src/main/java/io/github/ololx/leetcode/solutions/easy/task101/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/easy/task101/Solution.java
@@ -20,9 +20,45 @@ package io.github.ololx.leetcode.solutions.easy.task101;
 /**
  * 101. Symmetric Tree
  *
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ *
+ * Example 1:
+ * <p>Input: root = [1,2,2,3,4,4,3]
+ * Output: true</p>
+ *
+ * Example 2:
+ * <p>Input: root = [1,2,2,null,3,null,3]
+ * Output: false</p>
+ *
+ * Constraints:
+ * <ul>
+ *      <li>
+ *          The number of nodes in the tree is in the range [1, 1000]
+ *      </li>
+ *      <li>
+ *          -100 <= Node.val <= 100
+ *      </li>
+ * </ul>
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ *
+ * <b>Follow up:</b> Could you solve it both recursively and iteratively?
+ *
  * @author Alexander A. Kropotin
- * project leetcode-solutions
- * created 08/05/2024 11:37am
+ *     project leetcode-solutions
+ *     created 08/05/2024 11:37am
  */
 public class Solution {
 
@@ -34,7 +70,7 @@ public class Solution {
         return isSymmetric(root.left, root.right);
     }
 
-    public boolean isSymmetric(TreeNode left, TreeNode right) {
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
         if (left == null && right == null) {
             return true;
         } else if (left == null || right == null) {
---
+++ src/test/java/io/github/ololx/leetcode/solutions/easy/task101/SolutionTest.java
@@ -0,0 +1,73 @@
+package io.github.ololx.leetcode.solutions.easy.task101;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 08/05/2024 3:25pm
+ */
+public class SolutionTest {
+
+    @DataProvider(name = "providesTreeNodes")
+    public static Object[][] providesTreeNodes() {
+        return new Object[][]{
+            {
+                new Solution.TreeNode(
+                    1,
+                    new Solution.TreeNode(
+                        2,
+                        new Solution.TreeNode(3),
+                        new Solution.TreeNode(4)
+                    ),
+                    new Solution.TreeNode(
+                        2,
+                        new Solution.TreeNode(4),
+                        new Solution.TreeNode(3)
+                    )
+                ),
+                true
+            },
+            {
+                new Solution.TreeNode(
+                    1,
+                    new Solution.TreeNode(
+                        2,
+                        null,
+                        new Solution.TreeNode(4)
+                    ),
+                    new Solution.TreeNode(
+                        2,
+                        null,
+                        new Solution.TreeNode(3)
+                    )
+                ),
+                false
+            },
+            {
+                new Solution.TreeNode(
+                    1,
+                    new Solution.TreeNode(
+                        2,
+                        new Solution.TreeNode(3),
+                        new Solution.TreeNode(4)
+                    ),
+                    new Solution.TreeNode(
+                        2,
+                        new Solution.TreeNode(3),
+                        new Solution.TreeNode(4)
+                    )
+                ),
+                false
+            }
+        };
+    }
+
+    @Test(dataProvider = "providesTreeNodes")
+    public void isSymmetric_whenTreesIsSymmetric_thenReturnTrue(Solution.TreeNode p, boolean expected) {
+        assertEquals(new Solution().isSymmetric(p), expected);
+    }
+}
