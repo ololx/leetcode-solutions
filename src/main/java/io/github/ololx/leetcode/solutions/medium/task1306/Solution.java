05a21cf80f8ba001dee5c65728e60ba554624d5c
a-kropotin <a.kropotin@pixonic.com>
2024-05-08 09:44:16 +0300

    chore(Solution.java) format code

--- src/main/java/io/github/ololx/leetcode/solutions/medium/task1306/Solution.java
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task1306/Solution.java
@@ -22,7 +22,8 @@ import java.util.Arrays;
 /**
  * 1306. Jump Game III
  *
- * Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+ * Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are
+ * at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
  *
  * Notice that you can not jump outside of the array at any time.
  *
