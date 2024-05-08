74b4b4c8eed6969b909dbb5835a3a36c32b12bb9
a-kropotin <a.kropotin@pixonic.com>
2024-05-08 11:43:49 +0300

    feature(Solution.java) implement solution for task 101

---
+++ src/main/java/io/github/ololx/leetcode/solutions/easy/task101/Solution.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2024 the project leetcode-solutions authors
+ * and the original author or authors annotated by {@author}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.leetcode.solutions.easy.task101;
+
+/**
+ * 101. Symmetric Tree
+ *
+ * @author Alexander A. Kropotin
+ * project leetcode-solutions
+ * created 08/05/2024 11:37am
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else if (left.val != right.val) {
+            return false;
+        }
+
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
