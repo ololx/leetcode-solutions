42ed862ece226aa4eaf4d651e3aac81b3a10d39f
a-kropotin <a.kropotin@pixonic.com>
2024-09-17 19:09:50 +0300

    feature: init class Solution

---
+++ src/main/java/io/github/ololx/leetcode/solutions/medium/task1371/Solution.java
@@ -0,0 +1,9 @@
+package io.github.ololx.leetcode.solutions.medium.task1371;
+
+/**
+ * @author Alexander A. Kropotin
+ *     project leetcode-solutions
+ *     created 17/09/2024 7:08 pm
+ */
+public class Solution {
+}
